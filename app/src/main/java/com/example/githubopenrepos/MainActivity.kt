b960d0cad89b03335967bb0b28c407736148176b
Agrost <kovalev1998@mail.ru>
2021-09-26 23:18:22 +0300

    Change mistake with fragmentManager

--- app/src/main/java/com/example/githubopenrepos/MainActivity.kt
+++ app/src/main/java/com/example/githubopenrepos/MainActivity.kt
@@ -8,8 +8,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.container, HomeFragment())
-            .commitNow()
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, HomeFragment())
+                .commitNow()
+        }
     }
 }
